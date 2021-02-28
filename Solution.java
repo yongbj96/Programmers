395caaf4bf95901c0e082e2f356dd1070e182b8b
Bum-Jung, Yong <dydqjawnd@gmail.com>
2021-02-28 16:58:16 +0900

    push 20210228

--- Solution.java
+++ Solution.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        
         //solution
     }
 }
---
+++ level1/lessons_12937.java
@@ -0,0 +1,11 @@
+package level1;
+
+// 짝수와 홀수
+public class lessons_12937 {
+    /*
+        int num = 3;
+    */
+    public static String solution(int num) {
+        return num%2==0 ? "Even" : "Odd";
+    }
+}
